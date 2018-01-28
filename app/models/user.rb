abe48417251f7fd9309482642c16f5f378f70a31
shirafuji <shirashirayuyu@gmail.com>
2018-01-28 21:11:34 +0900

    Add user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: true}
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true, length: {minimum: 6}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,39 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+def setup
+  @user = User.new(name: "Yu", email: "aaa@aaa", password: "foobar", password_confirmation: "foobar")
+end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+  test "length of name should be appropriate" do
+    @user.email = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    assert_not @user.valid?
+  end
+  test "email should have valid format" do
+    @user.email = "aaa"
+    assert_not @user.valid?
+  end
+  test "users should be unique" do
+    dup_user = @user.dup
+    dup_user.email = @user.email.upcase
+    @user.save
+    assert_not dup_user.valid?
+  end
+  test "email should be downcase" do
+    @user.email = "AAA@aaa"
+    @user.save
+    assert_equal @user.email, "aaa@aaa"
+  end
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a"*5
+    assert_not @user.valid?
+  end
+  test "password should be present" do
+    @user.password = @user.password_confirmation = ""*7
+    assert_not @user.valid?
+  end
+
 end
